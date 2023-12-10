92204cc8db2826f99d864b1d67dd864043614f9e
erangag <erangagunarathna@gmail.com>
2023-12-10 22:53:29 +1300

    Added Customer API

---
+++ src/Services/Customer/Customer.API/Repositories/CustomerRepository.cs
@@ -0,0 +1,44 @@
+﻿using Customer.API.Entities;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace Customer.API.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly string _connectionString;        
+
+        public CustomerRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CustomerDBConnectionStrings") ?? string.Empty;
+        }
+
+        public async Task<CustomerProfile> GetCustomerByIdAsync(int id)
+        {
+            CustomerProfile result;
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                result = await connection.QueryFirstOrDefaultAsync<CustomerProfile>("SELECT * FROM Customer WHERE Id = @Id", new { Id = id });
+            }
+
+            return result;
+        }
+
+        public async Task<List<CustomerProfile>> GetCustomersAsync()
+        {
+            List<CustomerProfile> result;
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                result = (await connection.QueryAsync<CustomerProfile>("SELECT * FROM Customer")).ToList();
+            }
+
+            return result;
+        }        
+    }
+}
---
+++ src/Services/Customer/Customer.API/Repositories/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Customer.API.Entities;
+
+namespace Customer.API.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<CustomerProfile> GetCustomerByIdAsync(int id);
+
+        Task<List<CustomerProfile>> GetCustomersAsync();
+
+    }
+}
